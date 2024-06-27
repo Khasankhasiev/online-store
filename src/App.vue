24cadd64356d7b3f9a1a7e89de47ce3a405f5300
Khasan <mr.khasiev@list.ru>
2024-06-27 18:17:42 +0300

    подключил tailwind добавили иконки и изображения создали компонент mainHeader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
-<script setup></script>
+<template>
+  <div class="bg-white w-4/5 m-auto mt-14 h-screen rounded-xl shadow-xl">
+    <Header></Header>
+    <div>
+      <div class="relative m-10 border border-slate-100 rounded-xl">
+        <img class="absolute top-0 left-0" src="/like-2.svg" alt="like 2" />
+        <img src="/sneakers/sneakers-1.jpg" alt="Sneakers" />
+        <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+        <div class="flex justify between">
+          <div class="flex flex-col">
+            <span class="text-slate-400">Цена</span>
+            <b>1205 руб.</b>
+          </div>
+          <img src="/plus.svg" alt="Plus" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-<template></template>
+<script setup>
+import Header from '@/components/MainHeader.vue'
+</script>
 
 <style scoped></style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/cart.svg" alt="Cart" />
+        <b>1205 руб.</b>
+      </li>
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/heart.svg" alt="heart" />
+        <span> Закладки</span>
+      </li>
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/profile.svg" alt="profile" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
