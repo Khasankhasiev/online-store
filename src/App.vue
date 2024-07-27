1fa36d1d483d7da6b6e23e9977eb91d39e4e0f95
Khasan <mr.khasiev@list.ru>
2024-07-27 19:13:32 +0300

    добавлен функционал для поиска и для фильтрации

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,21 @@
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
         <div class="flex gap-4">
-          <select name="" id="" class="py-2 px-3 border rounded-md outline-none">
-            <option value="">По названию</option>
-            <option value="">По цене (дешевые)</option>
-            <option value="">По цене (дорогие)</option>
+          <select
+            @change="onChangeSelect"
+            name=""
+            id=""
+            class="py-2 px-3 border rounded-md outline-none"
+          >
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
 
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" />
             <input
+              @input="onChangeSearchInput"
               class="border rounded-md py-2 pl-12 pr-4 outline-none focus:border-gray-400"
               type="text"
               placeholder="Поиск..."
@@ -31,7 +37,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, reactive, watch } from 'vue'
 import axios from 'axios'
 
 import Header from '@/components/MainHeader.vue'
@@ -40,19 +46,46 @@ import Drawer from '@/components/CatalogDrawer.vue'
 
 const items = ref([])
 
-onMounted(async () => {
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
   // fetch('https://2e4a820111f9b349.mokky.dev/items')
   //   .then((res) => res.json())
   //   .then((data) => {
   //     console.log(data)
   //   })
   try {
-    const { data } = await axios.get('https://2e4a820111f9b349.mokky.dev/items')
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get('https://2e4a820111f9b349.mokky.dev/items', {
+      params
+    })
     items.value = data
   } catch (err) {
     console.log(err)
   }
-})
+}
+
+onMounted(fetchItems)
+
+watch(filters, fetchItems)
 </script>
 
 <style scoped></style>
